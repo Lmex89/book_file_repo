4dd926d9c3ff13ce94ff1c41e72fba70343ce232
Lmex89 <bjt.mex@gmail.com>
2023-01-22 16:45:11 -0600

    static file

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
