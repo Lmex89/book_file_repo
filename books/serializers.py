c34e72ea136d7b6eec20878bfa386d354b54cddd
Lmex89 <bjt.mex@gmail.com>
2023-02-13 19:15:16 -0600

    an updated

--- books/serializers.py
+++ books/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from .models import ImgPortada, Categorias
 
-
-class ImgPortadaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ImgPortada
-        fields = "__all__"
-        depth = 1
-
-
 class CategoryModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categorias
         fields = "__all__"
+class ImgPortadaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ImgPortada
+        fields = "__all__"
+
+
+
